e59f4100daa65f1bcaf462fcfe76ca2c3a44bb78
Yuriy Nikolaev <warlokk83@gmail.com>
2021-03-17 22:38:30 +0400

    Junit5 and tests added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,6 +1,8 @@
 package ru.netology.service;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
@@ -8,23 +10,47 @@ public class CashbackHackServiceTest {
     @org.junit.Test
     public void shouldRemainBelowBoundary() {
         int purchase = 800;
-        assertEquals(200, service.remain(purchase));
+        org.junit.Assert.assertEquals(200, service.remain(purchase));
     }
 
     @org.junit.Test
     public void shouldNotRemainEqualBoundary() {
         int purchase = 1000;
-        assertEquals(0, service.remain(purchase));
+        org.junit.Assert.assertEquals(0, service.remain(purchase));
     }
 
     @org.junit.Test
     public void shouldRemainAboveBoundary() {
         int purchase = 1500;
-        assertEquals(500, service.remain(purchase));
+        org.junit.Assert.assertEquals(500, service.remain(purchase));
     }
 
     @org.junit.Test
     public void shouldRemainBoundaryIfZeroPurchase() {
+        int purchase = 0;
+        org.junit.Assert.assertEquals(1000, service.remain(purchase));
+    }
+
+    @Test
+    public void shouldRemainBelowBoundary5() {
+        int purchase = 800;
+        assertEquals(200, service.remain(purchase));
+    }
+
+    @Test
+    public void shouldNotRemainEqualBoundary5() {
+        int purchase = 1000;
+        assertEquals(0, service.remain(purchase));
+    }
+
+    @Test
+    public void shouldRemainAboveBoundary5() {
+        int purchase = 1500;
+        assertEquals(500, service.remain(purchase));
+    }
+
+    @Test
+    public void shouldRemainBoundaryIfZeroPurchase5() {
         int purchase = 0;
         assertEquals(1000, service.remain(purchase));
     }
